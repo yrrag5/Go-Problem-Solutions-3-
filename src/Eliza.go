ad00210272e04bf5f260a6c185ea9f0decff1eae
yrrag5 <G00335806@gmit.ie>
2017-12-01 17:34:57 +0000

    Adding Q3 solution to capture input I am

--- src/Eliza.go
+++ src/Eliza.go
@@ -5,7 +5,7 @@ import (
     "fmt"
     "math/rand"
     "time"
-    // Regular expressions import 
+    // Regular Expressions import
     "regexp"
 )
 func ElizaResponse(input string) string {
@@ -16,7 +16,14 @@ func ElizaResponse(input string) string {
         return "Why don’t you tell me more about your father?"
     }
 
-  
+    // Q3
+    // [] - outside, * - none of these chars, any char, outside ? - 0 or 1
+    re:= regexp.MustCompile(`I am ([^.!?]*)[.!?]?`)
+    if matched := re.MatchString(input); matched {
+        return re.ReplaceAllString(input, "How do you know you are $1?")
+    }
+
+
     responses := []string{
       "I'm not sure what you’re trying to say. Could you explain it to me?",
       "How does that make you feel?",
@@ -30,6 +37,7 @@ func ElizaResponse(input string) string {
 
 func main(){
     rand.Seed(time.Now().UTC().UnixNano())
+    
     fmt.Println("Input: " + " People say I look like both my mother and father.")
     fmt.Println("Output: " + ElizaResponse(" People say I look like both my mother and father."))
     fmt.Println()
@@ -50,5 +58,26 @@ func main(){
     fmt.Println("Output: " + ElizaResponse(" My grandfather was French!"))
     fmt.Println()
 
-    // add I am inputs
+    // Q3
+    //  I am, I'm inputs and outputs
+
+    fmt.Println("Input: " + " I am happy")
+    fmt.Println("Output: " + ElizaResponse("I am happy"))
+    fmt.Println()
+
+    fmt.Println("Input: " + " I am not happy with your responses.")
+    fmt.Println("Output: " + ElizaResponse("I am not happy with your responses."))
+    fmt.Println()
+
+    
+    fmt.Println("Input: " + " I am not sure that you understand the effect that your questions are having on me.")
+    fmt.Println("Output: " + ElizaResponse("I am not sure that you understand the effect that your questions are having on me."))
+    fmt.Println()
+
+    
+    fmt.Println("Input: " + " I am supposed to just take what you’re saying at face value?")
+    fmt.Println("Output: " + ElizaResponse("I am supposed to just take what you’re saying at face value?"))
+    fmt.Println()
+
+
 } // Main
